bafa593b5b368cb26faa27ddaf88a23219b20b7d
Jo Sprague <josiah.sprague@gmail.com>
2020-04-09 22:08:00 +0100

    Initial Tater implementation
    A rough implementation of a component that wraps any other component
    allowing markers to be added at x and y percentages of the wrapped
    element's dimensions. Notes can be added to markers and everything is
    saved to localstorage.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,73 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from "react";
+import styled from "styled-components";
+import useLocalStorage from "./useLocalStorage";
+import Marker from "./marker";
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+const TaterFrame = styled.div`
+  outline: 2px dotted green;
+  display: inline-block;
+  position: relative;
+`;
+
+const Tater = ({ children: child, name }) => {
+  if (Array.isArray(child)) {
+    throw new Error("Tater only supports wrapping a single element");
+  }
+  const childWrapper = React.createRef();
+  const ns = name || child.type.displayName;
+  const [markers, setMarkers] = useLocalStorage(`${ns}-markers`, {});
+  const markerList = Object.keys(markers);
+  const handleClick = e => {
+    const { clientX, clientY, target } = e;
+    const isChildClick = childWrapper.current.contains(target);
+    if (isChildClick) {
+      const {
+        left,
+        top,
+        width,
+        height
+      } = childWrapper.current.getBoundingClientRect();
+      addMarker({
+        x: ((clientX - left) / width) * 100,
+        y: ((clientY - top) / height) * 100,
+        open: true
+      });
+    }
+  };
+  const addMarker = ({ x, y }) => {
+    const newId =
+      markerList.length > 0 ? Math.max.apply(Math, markerList) + 1 : 1;
+    const newMarkers = { ...markers };
+    newMarkers[newId] = { x, y };
+    setMarkers(newMarkers);
+  };
+  const setMessage = ({ message, id }) => {
+    const newMarkers = { ...markers };
+    newMarkers[id].message = message;
+    setMarkers(newMarkers);
+  };
+  const removeMarker = id => {
+    const newMarkers = { ...markers };
+    delete newMarkers[id];
+    setMarkers(newMarkers);
+  };
+
+  return (
+    <TaterFrame onClick={e => handleClick(e)}>
+      {markerList.map(id => {
+        return (
+          <Marker
+            key={id}
+            id={id}
+            {...markers[id]}
+            setMessage={msg => setMessage(msg)}
+            removeMarker={id => removeMarker(id)}
+          />
+        );
+      })}
+      <div ref={childWrapper}>{child}</div>
+    </TaterFrame>
+  );
+};
+
+export default Tater;
---
+++ src/marker.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Mark = styled.div`
+  height: 10px;
+  width: 10px;
+  background: red;
+  display: block;
+  position: absolute;
+`;
+
+const Tooltip = styled.div`
+  background: green;
+  display: block;
+  position: absolute;
+  top: 0;
+  left: calc(100% + 10px);
+`;
+
+const Marker = ({ x, y, id, message, setMessage, removeMarker }) => {
+  const [mode, setMode] = useState("hide");
+  const [toolTip, setTooltip] = useState(message);
+  const isEdit = mode === "edit";
+  const isView = mode === "view";
+  const isHide = mode === "hide";
+  const tooltipRef = React.createRef();
+  return (
+    <Mark
+      style={{ top: `${y}%`, left: `${x}%` }}
+      onMouseEnter={() => {
+        if (isHide) setMode("view");
+      }}
+      onClick={e => {
+        const hasTooltip = tooltipRef && tooltipRef.current;
+        const isTooltip = hasTooltip && tooltipRef.current.contains(e.target);
+        if (isView) {
+          setMode("edit");
+        } else if (!isTooltip) {
+          setMessage({ message: toolTip, id });
+          setMode("view");
+        }
+      }}
+      onMouseLeave={() => {
+        if (mode === "view") {
+          setMode("hide");
+        }
+      }}
+    >
+      {isEdit ? (
+        <Tooltip ref={tooltipRef}>
+          <textarea
+            placeholder="Add a note..."
+            value={toolTip}
+            onChange={e => setTooltip(e.target.value)}
+          />
+          <button onClick={() => removeMarker(id)}>Remove</button>
+        </Tooltip>
+      ) : isView ? (
+        <Tooltip>
+          <p>{toolTip || "Click on marker to edit"}</p>
+        </Tooltip>
+      ) : null}
+    </Mark>
+  );
+};
+
+export default Marker;
