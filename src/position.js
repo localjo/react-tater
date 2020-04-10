4a4f4ac705b6d15e2053d247c36cc89b97222580
Jo Sprague <josiah.sprague@gmail.com>
2020-04-10 14:13:30 +0100

    Add position validation
    This is an initial implentation of position validation that attempts to
    move markers if they collide with existing markers. It doesn't work very
    well, so it should be replaced by a grid system and an algorithm that
    checks the grid for open space.

---
+++ src/position.js
@@ -0,0 +1,76 @@
+export const detectCollision = function(a, b) {
+  const isCollision = !(
+    a.top >= b.bottom ||
+    a.right <= b.left ||
+    a.bottom <= b.top ||
+    a.left >= b.right
+  );
+  const overlap = {
+    bottom: a.bottom > b.top && a.bottom < b.bottom ? a.bottom - b.top : 0,
+    left: a.left > b.left && a.left < b.right ? b.right - a.left : 0,
+    top: a.top > b.top && a.top < b.bottom ? b.bottom - a.top : 0,
+    right: a.right > b.left && a.right < b.right ? a.right - b.left : 0
+  };
+  const x = overlap.left - overlap.right;
+  const y = overlap.top - overlap.bottom;
+  const smallerX = Math.abs(x) < Math.abs(y);
+  const adjustment = {
+    ...(smallerX ? { x: x || 1 } : { y: y || 1 })
+  };
+  return isCollision ? adjustment : false;
+};
+
+const getBox = function({ x, y, height, width, space }) {
+  const top = height * (y / 100);
+  const left = width * (x / 100);
+  return {
+    top,
+    left,
+    bottom: top + space,
+    right: left + space
+  };
+};
+
+export const findSafePosition = function(marker, container, others, depth = 0) {
+  console.log(depth);
+  const { height, width } = container;
+  const { x, y, space } = marker;
+  const top = height * (y / 100);
+  const left = width * (x / 100);
+  const newMarker = { ...marker };
+  if (left + space > width) {
+    newMarker.x = ((width - space) / width) * 100;
+  }
+  if (top + space > height) {
+    newMarker.y = ((height - space) / height) * 100;
+  }
+  const markerBox = getBox({
+    ...newMarker,
+    ...container
+  });
+  const adjustments = others
+    .map(m => {
+      const box = getBox({
+        ...m,
+        ...container,
+        space: marker.space
+      });
+      return detectCollision(markerBox, box);
+    })
+    .filter(n => n);
+  console.log({ adjustments });
+  if (adjustments.length > 0) {
+    const percentX = adjustments[0].x ? (adjustments[0].x / width) * 100 : 0;
+    const percentY = adjustments[0].y ? (adjustments[0].y / height) * 100 : 0;
+    const adjustedMarker = {
+      x: newMarker.x + percentX + depth,
+      y: newMarker.y + percentY + depth,
+      space
+    };
+    depth += 1;
+    return depth < 3
+      ? findSafePosition(adjustedMarker, container, others, depth)
+      : false;
+  }
+  return { x: newMarker.x, y: newMarker.y };
+};
